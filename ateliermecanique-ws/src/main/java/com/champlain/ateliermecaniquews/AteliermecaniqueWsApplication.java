ee08c0976c2f341000b32c5f5c42303dba4ed97c
MichaelVacca <michaelvacca07@gmail.com>
2024-02-14 03:05:25 -0500

    new cors

--- ateliermecanique-ws/src/main/java/com/champlain/ateliermecaniquews/AteliermecaniqueWsApplication.java
+++ ateliermecanique-ws/src/main/java/com/champlain/ateliermecaniquews/AteliermecaniqueWsApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AteliermecaniqueWsApplication {
@@ -15,5 +17,16 @@ public class AteliermecaniqueWsApplication {
     public static void main(String[] args) {
         SpringApplication.run(AteliermecaniqueWsApplication.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+
 
 }
