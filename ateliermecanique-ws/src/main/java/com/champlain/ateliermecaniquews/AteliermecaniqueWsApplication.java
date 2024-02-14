4df1445d66c0d36ce2676fd4c13d683490021df3
MichaelVacca <michaelvacca07@gmail.com>
2024-02-14 03:13:06 -0500

    new cors

--- ateliermecanique-ws/src/main/java/com/champlain/ateliermecaniquews/AteliermecaniqueWsApplication.java
+++ ateliermecanique-ws/src/main/java/com/champlain/ateliermecaniquews/AteliermecaniqueWsApplication.java
@@ -23,10 +23,15 @@ public class AteliermecaniqueWsApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**")
+                        .allowedOrigins("https://react-atelier.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
+
 
 
 }
